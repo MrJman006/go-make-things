d73f8a2939402c6669c7d3561e694337555c0c53
Joey C. DiGiorgio <digiorgiojoey@gmail.com>
2023-04-06 17:32:20 -0400

    Updated the deployment script.

--- deploy.sh
+++ deploy.sh
@@ -4,37 +4,55 @@ echo "$@" | grep -Pq "(^|\s+)(-s|--help)(\s+|$)"
 
 if [ $? -eq 0 ] || [ $# -ne 1 ]
 then
-    echo "usage: deploy.sh <directory>"
+    echo "usage: deploy.sh <project-directory>"
+    exit 1
+fi
+
+PROJECT_DIR="$(echo "${1}" | sed -r "s|(.)/+$|\1|")"
+
+#
+# Ensure there repo is up to date.
+#
+
+git status | grep -Pq "Changes not staged for commit"
+RESULT=$?
+if [ ${RESULT} -eq 0 ]
+then
+    echo "The repo has non-commited changes. Either discard them or stash them before attempting to deploy."
     exit 1
 fi
 
 #
-# Modify url paths to work with Github Pages.
+# Stage the target directory.
 #
 
-while read FILE
-do
-    echo "${FILE}"
-    sed -ri "s|href=\"(.*)\"|href=\"gmt-webapps-workshop/\1\"|" "${FILE}"
-    sed -ri "s|src=\"(.*)\"|href=\"gmt-webapps-workshop/\1\"|" "${FILE}"
-done < <(find "${1}" -type f)
+rsync -ai "${PROJECT_DIR}/" _stage
 
 #
 # Remove unnecessary files.
 #
 
-rm "${1}/README.md"
+rm "_stage/README.md"
+rm "_stage/package.json"
 
 #
-# Push the contents of the subdirectory to the 'gh-pages' remote branch.
+# Add the staging dir to the repo.
+#
+
+ORIGINAL_HEAD="$(git rev-parse --short HEAD)"
+git add --force _stage
+git commit -m "Deploying staged site files."
+
+#
+# Push the contents of the staging dir to the 'gh-pages' remote branch.
 #
 
 git push -d origin gh-pages
-git subtree push --prefix "${1}" origin gh-pages
+git subtree push --prefix _stage origin gh-pages
 
 #
-# Discard deployment changes.
+# Restore the git repo to it's original state.
 #
 
-git checkout -- .
+git reset --hard "${ORIGINAL_HEAD}"
 
