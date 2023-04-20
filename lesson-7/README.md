e0cf0863bfd8c4218adea1e20a3a107f581f3080
Joey C. DiGiorgio <digiorgiojoey@gmail.com>
2023-04-20 11:35:51 -0400

    Added the base files for lesson-7.

---
+++ lesson-7/README.md
@@ -0,0 +1,6 @@
+# Lesson 7 - More On Accessiblity
+
+Lesson 7 goes over additional concepts for accessibility.
+
+Lesson 7 has us implement remove from cart with accessibility in mind.
+
---
+++ lesson-7/modules/cart.js
@@ -0,0 +1,63 @@
+////////////////////////////////
+// Imports
+
+import { Storage } from "./storage.js"
+import { store } from "../vendors/reef/reef.es.min.js"
+
+////////////////////////////////
+// Cart API
+
+function Cart()
+{
+    let _CACHE_ID = "cart";
+    let _cart;
+
+    function load()
+    {
+        console.log("Loading cart from the site cache.");
+        _cart = store(Storage.local.getItem(_CACHE_ID) || []);
+    }
+    
+    function addProduct(productId)
+    {
+        if(_cart.includes(productId))
+        {
+            return;
+        }
+
+        console.log("Adding product to the cart.");
+        _cart.push(productId);
+        Storage.local.setItem(_CACHE_ID, _cart);
+    }
+    
+    function hasProduct(productId)
+    {
+        return _cart.includes(productId);
+    }
+
+    function productCount()
+    {
+        return _cart.length;
+    }
+
+    function forEachProduct(callback)
+    {
+        _cart.forEach(callback);
+    }
+
+    return {
+        load,
+        addProduct,
+        hasProduct,
+        productCount,
+        forEachProduct
+    };
+}
+
+////////////////////////////////
+// Exports
+
+export {
+    Cart
+};
+
