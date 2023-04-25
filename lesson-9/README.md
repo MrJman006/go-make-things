b936c250bb00e682fa66d2be390b930de750840a
Joey C. DiGiorgio <digiorgiojoey@gmail.com>
2023-04-25 11:01:00 -0400

    Added base files for lesson 9.

---
+++ lesson-9/README.md
@@ -0,0 +1,7 @@
+# Lesson 9 - Middleman APIs
+
+Lesson 9 goes over middleman APIs and how we can use Cloudflare FAAS workers
+as a middleman.
+
+Lesson 9 has us implement a middleman API to accept payment using Stripe.
+
---
+++ lesson-9/modules/product-list.js
@@ -0,0 +1,92 @@
+////////////////////////////////
+// Imports
+
+import { Storage } from "./storage.js"
+
+////////////////////////////////
+// Products API
+
+function ProductList()
+{
+    let _PRODUCTS_ENDPOINT = "https://gmt-webapps-workshop.cfjcd.workers.dev";
+    let _CACHE_ID = "productList";
+    let _productList = [];
+
+    async function load(options = { bypassCache: false })
+    {
+        if(!options.bypassCache)
+        {
+            _productList = Storage.session.getItem(_CACHE_ID);
+            if(_productList)
+            {
+                console.log("Loading product list from the session cache.");
+                return;
+            }
+        }
+    
+        try
+        {
+            console.log("Fetching fresh product list from the server.");
+            let result = await fetch(_PRODUCTS_ENDPOINT);
+            if(!result.ok)
+            {
+                throw result;
+            }
+        
+            _productList = await result.json();
+            Storage.session.setItem(_CACHE_ID, _productList);
+            return;
+        }
+        catch(error)
+        {
+            console.warn(error);
+    
+            _productList = [];
+            return;
+        }
+    }
+
+    function save()
+    {
+        console.log("Saving product list to the session cache.");
+        Storage.session.setItem(_CACHE_ID, _productList);
+    }
+
+    function length()
+    {
+        return _productList.length;
+    }
+
+    function forEach(callback)
+    {
+        _productList.forEach(callback);
+    }
+
+    function get(productId)
+    {
+        return _productList.find(function(p){ return p.id == productId; });
+    }
+
+    function contains(productId)
+    {
+        let product = _productList.find(function(p){ return p.id == productId; });
+        return (product) ? true : false;
+    }
+
+    return {
+        load,
+        save,
+        length,
+        forEach,
+        get,
+        contains
+    };
+}
+
+////////////////////////////////
+// Exports
+
+export {
+    ProductList
+};
+
