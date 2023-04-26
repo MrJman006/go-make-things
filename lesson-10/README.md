3a42b7b6093b0d30d2d4e7fc68c27937b8763fdc
Joey C. DiGiorgio <digiorgiojoey@gmail.com>
2023-04-25 22:22:53 -0400

    Added the base files for lesson 10.

---
+++ lesson-10/README.md
@@ -0,0 +1,8 @@
+# Lesson 10 - HTTP Methods And FAAS Storage
+
+Lesson 10 goes over HTTP headers and how to allow/restrict different HTTP
+methods for FAAS functions. Persistant storage is also covered.
+
+Lesson 10 has us move the photo data into a Cloudflare database (called
+KV in Cloudflare lingo) and use that in our FAAS functions.
+
---
+++ lesson-10/modules/notifier.js
@@ -0,0 +1,40 @@
+import { store, component } from "../vendors/reef/reef.es.min.js"
+
+let notifier = document.querySelector("[data-notifier]");
+let notifications = store([]);
+component(
+    notifier,
+    function()
+    {
+        let template = ``;
+
+        notifications.forEach(
+            function(message)
+            {
+                template += `<p><em>${message}</em></p>`;
+            }
+        );
+
+        return template;
+    }
+);
+
+function notify(message)
+{
+    notifications.push(message);
+
+    setTimeout(
+        function(e)
+        {
+            let index = notifications.indexOf(message);
+            notifications.splice(index, 1);
+        },
+        3000
+    );
+}
+
+window.notify = notify;
+export {
+    notify
+};
+
